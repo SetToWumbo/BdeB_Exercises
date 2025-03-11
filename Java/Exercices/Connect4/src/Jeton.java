68762f743d862cc6b88a39f9ff76cf6a0146f435
nikola kisiov <nikolakisiov@nikolas-MacBook-Air.local>
2025-03-11 18:07:55 -0400

    feat: Fonction placer jeton

--- Java/Exercices/Connect4/src/Jeton.java
+++ Java/Exercices/Connect4/src/Jeton.java
@@ -1,11 +1,12 @@
 public enum Jeton {
     ROUGE( "\u001B[31m"+  "*" + "\u001B[0m"),
     JAUNE( "\u001B[33m" + "*" + "\u001B[0m"),
-   VIDE("\u001B[0m" + "_");
-    public String jeton;
+    NOIR("\u001B[30m" + "_" +  "\u001B[0m"),
+   VIDE("\u001B[30m"  + "_" + "\u001B[0m");
+    public String type;
 
     Jeton(String type) {
-        this.jeton = jeton;
+        this.type = type;
     }
 
 }
--- Java/Exercices/Connect4/src/Main.java
+++ Java/Exercices/Connect4/src/Main.java
@@ -1,9 +1,11 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static int ROW = 6;
     public static int COLUMN = 7;
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -22,48 +24,69 @@ public class Main {
         afficherJeu(planJeu);
         Player gagnant = null;
         Player quiJoue = player2;
-
+//        do{
         quiJoue = (quiJoue == player1) ? player2 : player1;
         System.out.println("C'est le tour de " + quiJoue.getNom());
 
-        placerJeton(quiJoue, planJeu);
-
         planJeu[3][5] = Jeton.JAUNE;
         planJeu[3][6] = Jeton.ROUGE;
-        afficherJeu(planJeu);
+        placerJeton(quiJoue, planJeu);
+
+
+
+//        }while(gagnant == null);
+
+
     }
 
     public static boolean placerJeton(Player quiJoue, Jeton[][] planJeu) {
-        int colonne = saisirChoix();
+        int colonne = saisirChoix(planJeu);
+        for(int row = 0; row < ROW; row++){
+            if(planJeu[row][colonne] != Jeton.VIDE ){
+                return true;
+            }
+            planJeu[row][colonne] = quiJoue.jeton;
+            afficherJeu(planJeu);
+            try {
+                TimeUnit.MILLISECONDS.sleep(500);
+            }catch(Exception e){
 
+                }
+        }
         return false;
     }
 
-    public static int saisirChoix() {
+
+    public static int saisirChoix(Jeton[][]planJeu) {
         Scanner scanner = new Scanner(System.in);
         int colonne = -1;
         do {
             System.out.print("Dans quelle colonne voulez-vous mettre un jeton? [1 a 7] : ");
             String saisie = scanner.nextLine().trim();
             try {
-                colonne = validerChoix(saisie);
-            } catch (Exception e) {
+                colonne = validerChoix(saisie, planJeu);
+            } catch (Exception e ) {
                 System.out.println(e);
             }
         } while (colonne < 1 || colonne > 7);
+        System.out.println(colonne);
         return colonne;
     }
 
-    public static int validerChoix(String choix) {
+    public static int validerChoix(String choix, Jeton[][]planJeu) {
         int colonne = 0;
         try {
             colonne = Integer.parseInt(choix) - 1;
         } catch (InputMismatchException e) {
             System.out.println("Avez-vous saisi un nombre?");
         }
-        if(colonne < 1 || colonne > 7){
+        if (colonne < 1 || colonne > 7) {
             throw new IllegalArgumentException("Erreur d'entree - saisir un nombre entre 1 et 7");
         }
+        if( planJeu[0][colonne] != Jeton.VIDE){
+         throw new IllegalArgumentException("La colonne est pleine! vous ne pouvez pas y mettre un jeton.\nEssayez de nouveau.");
+        }
+
         return colonne;
     }
 
@@ -73,7 +96,7 @@ public class Main {
 
             for (int col = 0; col < COLUMN; col++) {
                 System.out.print("|");
-                System.out.print(planJeu[row][col].jeton);
+                System.out.print(planJeu[row][col].type);
                 System.out.print("|");
             }
             System.out.println();
